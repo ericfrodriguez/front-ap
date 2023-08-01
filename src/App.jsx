299a6ce7ef3c26212fe098e69c5d60028116ba4c
Eric Rodriguez <feric.rodriguez@gmail.com>
2023-08-01 08:58:15 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <header>
+        <h1>Holis</h1>
+      </header>
+      <main></main>
+      <footer></footer>
+    </>
+  )
+}
+
+export default App
